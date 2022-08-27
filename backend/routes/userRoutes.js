1c464098f6a92510d56b2724df59431391a81338
praveenpugazh <praveenpugazh14@gmail.com>
2022-08-27 16:44:22 +0530

    jwt second commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, LoginUser } = require('../controllers/userController')
+
 router.post('/', registerUser)
 
 router.post('/login', LoginUser)
